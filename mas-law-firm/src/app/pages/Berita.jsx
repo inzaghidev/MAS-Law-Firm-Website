20ce8ef25046ffb175b3f06e93e8f16c74adc547
inzaghidev <izzumiposhaf29@gmail.com>
2026-03-12 10:08:52 +0700

    Edited some Files

--- mas-law-firm/src/app/pages/Berita.jsx
+++ mas-law-firm/src/app/pages/Berita.jsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
 import { Calendar, ArrowRight } from "lucide-react";
 import { Card, CardContent } from "../components/ui/card";
+// import { supabase } from "../../lib/supabase";
+// import { Link } from "react-router-dom";
 
 export function Berita() {
   const news = [
